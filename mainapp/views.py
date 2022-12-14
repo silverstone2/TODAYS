846c68d4bd5e5e75734192d15e88d16b5e0d7a1a
Sayh0 <michaelyoon0948@gmail.com>
2022-12-14 11:37:55 +0900

    testproject 의 레이아웃 적용 작성자 윤성훈 2022/12/14

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-def MainFunc(request):
-    return render(request, 'index.html')
+# def MainFunc(request):
+#     return render(request, 'index.html')
+
+
+def mainapp(request):
+    # 임시 날씨에 따라 배경화면 바뀔 예정
+    background = "static/videos/rainy.mp4"
+    context = {
+        'background': background
+    }
+    return render(request, 'mainapp/main.html', context)
