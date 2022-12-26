829e5d161561407a3ac4a769d96a89d1a38c6cb4
admin <dh8570@naver.com>
2022-12-26 10:13:43 +0900

    model 적용

--- mainapp/apps.py
+++ mainapp/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from keras.models import load_model
 
+models = []
 
 class MainappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mainapp"
+
+    def ready(self):
+        model = load_model('./mainapp/DL_model/최종_MLP.hdf5')
+        models.append(model)
+        print(models[0])
+        pass
